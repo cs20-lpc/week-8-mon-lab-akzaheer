20a66eb96c58aa3dbe509deeb2be50db5d977588
ayaanzaheer <ayaanzaheer@gmail.com>
2026-03-11 14:51:13 -0700

    final

--- lab09-main.cpp
+++ lab09-main.cpp
@@ -4,30 +4,26 @@ using namespace std;
 
 int main() {
 
-    ArrayStack<int> s(3);           // ints
-    cout << "Push stuff\n";
+    ArrayStack<int> s(3);
     s.push(1);
-    s.push(-67);          //puush
+    s.push(-67);
     s.push(34);
     cout << s << endl;
-    cout << "Peek: " << s.peek() << endl;
-    s.pop();
-    cout << "After pop:\n";
-    cout << s << endl;
-    cout << "Before rotate:\n";
-    cout << s << endl;
-    s.rotate(Stack<int>::RIGHT);
-    cout << "After R rotate:\n";
-    cout << s << endl;
-    s.rotate(Stack<int>::LEFT);
-    cout << "After L rotate:\n";
-    cout << s << endl;
-    cout << "\nString stack\n";
 
-    ArrayStack<string> s2(3);               // strings
+    s.pop();
+    cout << s << endl;
+
+    s.rotate(Stack<int>::RIGHT);
+    cout << s << endl;
+
+    s.rotate(Stack<int>::LEFT);
+    cout << s << endl;
+
+    ArrayStack<string> s2(3);
     s2.push("lebron");
-    s2.push("jordan");          // add and then print to show it works
+    s2.push("jordan");
     s2.push("kobe");
     cout << s2 << endl;
+
     return 0;
 }
